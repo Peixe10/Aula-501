637cb20f0bae23d989bc10138dbcd4cf641cac71
Peixe <thefish0987@hotmail.com>
2018-07-30 16:45:06 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -1,46 +1,67 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const expressMongoDb = require('express-mongo-db');
 
-app.use(expressMongoDb('mongodb://localhost/emails'));
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static('static'));
+const app = express(); //Essa função importada da linha 1 cria um DAEMON, que está importando na rede.
 
-// const cadastros = {};
+//Use adiciona um recurso aquela a aplicação
+app.use(expressMongoDb('mongodb://localhost/emails')); //Importada na linha 4 ele serve pra conectar o express e o mongo, e passa qual o endereço que está conectando. (localhost em e-mails.).
+app.use(cors()); //Cors é CORS ORIGIN RESOURCE SHERE. Sem o cors voce pode ligar o backend e o front na mesma máquina, mas se estiverem em lugares diferentes você não poderá conectar. Quando você usa só o cors ela é publica, agora se quiser deixar privada coloca o nome e o IP para validar no CORS. Muitas vezes as API's prontas da internet dão problemas no cors.
+app.use(bodyParser.json()); //Converter automaticamente o corpo de requisições (bodyParser) e respostas para JSON. TODAS as suas APIs utilizam o JSON terão que ter isso.
+app.use(express.static('static')); //Express Static é onde irá atingir o front end e o static é o nome da pasta que está o arquivo front end.
 
-app.get('/emails', (req, res) => {
-    req.db.collection('cadastros').find({}).toArray((err, cadastros) => {
-        res.send(cadastros);
+/*A partir daqui começa a se verificas os end points, no caso faremos isso dos NOMES e EMAILS. O padrão é APP. MÉTODO DE HTTP. Utilizamos o padrão rest que so tem como metodo os GET (obter) POST (escrever) PUT () DELETE (apagar).
+
+CRUD (Create (post) Read (get) Update (post) Delete (delete)) são funcionalidades que usam os metodos de HTTP entre parenteses, e para fazer a relação com o banco de dados ....*/
+
+app.get('/emails', (req, res) => { //Primeiro parametro é um URI (/email), e o call back é requisição e resposta. Em req tem acesso ao que o front end enviou para você e o no res a resposta ddo backend.
+    req.db.collection('cadastros').find({}).toArray((err, cadastros) => { //Só faz sentido por causa da linha 9. COLLECTION é um conjunto de documentos que fazem sentido juntos. A collection "cadastros" é do MONGO, se não tiver nenhuma lá ele criará uma com esse nome. FIND retorna uma sequencia de itens de uma coleção e o toArray transforma isso em um array de objetos para retornarmos para o front end(java script), além disso se passa um callback de (err, cadastro) (que no caso não esta passando nenhum erro)
+        res.send(cadastros);//Esta enviando a linha de respostas
     });
 });
 
 app.get('/email/:nome', (req, res) => {
-    // let email = cadastros[req.params.nome];
 
-    // if(!email){
-    //     return res.status(404).send({mensagem: `Lamentamos, mas não encontramos ${req.params.nome}.`})
-    // }
+    //findOne -> só vai retornar o primeiro nome
 
     req.db.collection('cadastros').findOne({nome: req.params.nome}, (err, cadastro) => {
         return res.send(cadastro);
     });
 });
 
-app.post('/cadastrar', (req, res) => {
+app.post('/email/cadastrar', (req, res) => {
     if(!req.body.nome || !req.body.email){
         return res.status(400).send({mensagem: "Nome e email são obrigatórios"});
+
+        //Quando da esse send o formato que ele está enviando é JSON, pois foi colocado que o bodyParser é JSON lá em cima.
     }
 
-    // cadastros[req.body.nome] = req.body.email;
-
     req.db.collection('cadastros').insert(req.body, (err) => {
-        console.log(err);
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.send({mensagem: 'Cadastro realizado com sucesso!'});
+        }
     });
-
-    res.send({mensagem: 'Cadastro realizado com sucesso!'});
 });
 
+app.delete('/email/:nome'), (req, res) => {
+
+    if(!req.params.nome){
+        return res.status(400).send({mensagem: "Nome é obrigatório"});
+    }
+    req.db.collection('cadastros').deleteOne({nome: req.params.nome}, err => {
+
+        if(err){
+            console.log(err);
+        }
+        else{
+            return res.send({mensagem: "Usuario removido com sucesso"});
+        }
+    });
+}
+
+//App listen indica o listen da aplicação.
 app.listen(3000, () => console.log('Aplicação iniciada.'));
--- static/scripts.js
+++ static/scripts.js
@@ -3,40 +3,72 @@ let campoEmail = document.querySelector('#email');
 let botao = document.querySelector('button');
 let lista = document.querySelector('ul');
 
+function apagarItem(){
+
+    let texto = this.previousElementSibling.innerHTML; //Colocou o innerHTML pois ele pega só o texto, mas só quero o nome.
+    let nome = texto.split("-")[0];
+
+    fetch(`http://localhost:3000/email/${nome.trim()}`,{
+        method: 'DELETE'}).then(() => {
+        carregar(); //Após apagar recarrega a página.
+    })
+}
+
 botao.addEventListener('click', () => {
     let cadastro = {
+        
+        //Coloca o nome na variável nome (o mesmo com email)
         nome: campoNome.value,
         email: campoEmail.value
     }
-
+    
+    //Esvaziar os campos no HTML:
+    
     campoNome.value = '';
     campoEmail.value = '';
-
-    fetch('http://10.162.105.176:3000/cadastrar', {
-        method: 'POST',
-        body: JSON.stringify(cadastro),
-        headers: {
-            'Content-type': 'application/json'
-        }
-    }).then((resposta) => {
-        return resposta.json();
-    }).then((dados) => {
-        
-    });
+    
+    //Padrão exemplo de FETCH que sempre é utilizado
+    
+    fetch('http://localhost:3000/email/cadastrar', {
+    method: 'POST', //Para o get não precisa preencher tudo isso aqui.
+    body: JSON.stringify(cadastro),
+    headers: {
+        'Content-type': 'application/json'
+    }
+}).then(() => {
+    carregar();   //Serve para atualizar os dados automaticamente
+});
 });
 
-function carregar(){
-    fetch('http://10.162.105.176:3000/emails').then((resposta) => {
-        return resposta.json();
-    }).then((cadastros) => {
-        let html = '';
+//Função carregar: está fazendo um fetch também. Mas como está fazendo com GET não precisa de todo aquele texto.
 
-        for(let cadastro of cadastros){
-            html += `<li>${cadastro.nome} - ${cadastro.email}</li>`;
-        }
-
-        lista.innerHTML = html;
-    });
+// ACESSA LOCAL HOST ACESSA O JSON LIMPA A TELA PARA CADA CADASTRO DA LISTA DE CADASTRO ELA MONTATELA AQUELE CADASTRO.
+function montarTela(cadastro){
+    let entrada = document.createElement('li'); //Cria um elemento LINHA
+    let texto = document.createElement('span');
+    texto.innerHTML = `${cadastro.nome} - ${cadastro.email}`;
+    let botao = document.createElement('button');
+    botao.innerHTML = "Apagar"; //Cria BOTAO
+    botao.addEventListener('click', apagarItem);
+    
+    entrada.appendChild(texto);
+    entrada.appendChild(botao);
+    lista.appendChild(entrada); //Acrescenta o elemento na lista de filhos.
 }
 
-setInterval(carregar, 3000);
+
+function carregar(){
+    fetch('http://localhost:3000/emails').then(resposta => {
+    return resposta.json();
+}).then(cadastros => {
+    
+    lista.innerHTML = ''; //Limpa a lista para não ficar escrevendo todos os blocos de código novamente.
+    
+    for(let cadastro of cadastros){
+        montarTela(cadastro); 
+    }
+});
+}
+
+
+carregar();
